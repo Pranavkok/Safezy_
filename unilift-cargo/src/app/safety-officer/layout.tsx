e44b77e1db4ce23cf5cc1b5bc46e74749971fc8a
Akash Nair <68143797+AkashNair-o@users.noreply.github.com>
2026-04-07 19:13:47 +0530

    Update layout.tsx

--- unilift-cargo/src/app/safety-officer/layout.tsx
+++ unilift-cargo/src/app/safety-officer/layout.tsx
@@ -1,6 +1,8 @@
 import SafetyOfficerDashboardLayout from '@/layouts/SafetyOfficerDashboardLayout';
 import { ReactNode } from 'react';
 
+export const dynamic = 'force-dynamic';
+
 export default function SafetyOfficerRootLayout({
   children
 }: Readonly<{
