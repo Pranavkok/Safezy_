ab5cc5f9855d811a78d77cccbce43ed627199a21
Akash Nair <68143797+AkashNair-o@users.noreply.github.com>
2026-04-07 19:12:11 +0530

    Update layout.tsx

--- unilift-cargo/src/app/contractor/layout.tsx
+++ unilift-cargo/src/app/contractor/layout.tsx
@@ -1,6 +1,8 @@
 import ContractorDashboardLayout from '@/layouts/ContractorDashboardLayout';
 import { ReactNode } from 'react';
 
+export const dynamic = 'force-dynamic';
+
 export default function ContractorRootLayout({
   children
 }: Readonly<{
