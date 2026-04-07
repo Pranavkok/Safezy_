bf0d951699c4c213ce0595badbf2d7b106f64cc6
Akash Nair <68143797+AkashNair-o@users.noreply.github.com>
2026-04-07 19:10:40 +0530

    Add dynamic export to AdminRootLayout

--- unilift-cargo/src/app/admin/layout.tsx
+++ unilift-cargo/src/app/admin/layout.tsx
@@ -1,6 +1,8 @@
 import AdminDashboardLayout from '@/layouts/AdminDashboardLayout';
 import { ReactNode } from 'react';
 
+export const dynamic = 'force-dynamic';
+
 export default function AdminRootLayout({
   children
 }: Readonly<{
