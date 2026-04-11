0ac89846eced42692f80c27258002206ee51bf62
pranavkokoff <pranavkokoff@gmail.com>
2026-04-11 18:45:13 +0530

    Update Capacitor server URL

--- unilift-cargo/capacitor.config.ts
+++ unilift-cargo/capacitor.config.ts
@@ -5,7 +5,7 @@ const config: CapacitorConfig = {
   appName: 'Safezy',
   webDir: 'public', // Leave empty since there's no static export
   server: {
-    url: 'http://192.168.29.192:3000', // Hosted Next.js app URL
+    url: 'https://safezy-989s.vercel.app/', // Hosted Next.js app URL
     cleartext: true, // Optional: Only set to true if using HTTP during development
     allowNavigation: ['*.payu.in', 'secure.payu.in', 'api.payu.in']
   },
