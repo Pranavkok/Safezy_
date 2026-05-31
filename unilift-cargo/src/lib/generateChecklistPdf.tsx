81bd457f926de13f5088e6199c738944be4048e2
pranavkokoff <pranavkokoff@gmail.com>
2026-05-31 11:17:30 +0530

    fix: read pdf ReadableStream into Buffer using stream/consumers

--- unilift-cargo/src/lib/generateChecklistPdf.tsx
+++ unilift-cargo/src/lib/generateChecklistPdf.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import { buffer as streamToBuffer } from 'stream/consumers';
 import { pdf } from '@react-pdf/renderer';
 import ChecklistPdfDocument, { ChecklistPdfData } from '@/data/ChecklistPdfDocument';
 
 export async function generateChecklistPdfBase64(data: ChecklistPdfData): Promise<string> {
   const element = ChecklistPdfDocument({ data });
   const instance = pdf(element as any);
-  const buffer = await instance.toBuffer();
-  return Buffer.from(buffer).toString('base64');
+  const stream = await instance.toBuffer();
+  const buf = await streamToBuffer(stream as any);
+  return buf.toString('base64');
 }
