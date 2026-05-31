3a6fe03064f5d739462ce44a8ba4dd589c25afdb
pranavkokoff <pranavkokoff@gmail.com>
2026-05-31 11:11:13 +0530

    fix: call PDF component directly to get Document element for pdf()

--- unilift-cargo/src/lib/generateChecklistPdf.tsx
+++ unilift-cargo/src/lib/generateChecklistPdf.tsx
@@ -3,8 +3,8 @@ import { pdf } from '@react-pdf/renderer';
 import ChecklistPdfDocument, { ChecklistPdfData } from '@/data/ChecklistPdfDocument';
 
 export async function generateChecklistPdfBase64(data: ChecklistPdfData): Promise<string> {
-  const element = React.createElement(ChecklistPdfDocument, { data });
-  const instance = pdf(element);
+  const element = ChecklistPdfDocument({ data });
+  const instance = pdf(element as any);
   const buffer = await instance.toBuffer();
   return Buffer.from(buffer).toString('base64');
 }
